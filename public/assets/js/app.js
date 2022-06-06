3785d8b2ca88961ad0e9d56ae5ce2eeb2c2cd422
py <pyd3.14@gmail.com>
2022-06-07 01:32:37 +0200

    init app.js & install fosjrouting bundle

---
+++ public/assets/js/app.js
@@ -0,0 +1,14 @@
+var app = {
+
+    init: function () {
+
+        /**
+        * *****************************
+        * L I S T E N E R S
+        * *****************************
+        */
+    }
+}
+
+// AppWitch Loading
+document.addEventListener('DOMContentLoaded', app.init)
