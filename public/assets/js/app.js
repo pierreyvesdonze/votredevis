b39417961312b75b8f452f7630897fb0576e0b6b
py <pyd3.14@gmail.com>
2022-06-11 14:34:26 +0200

    add searchbar

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,9 +7,25 @@ var app = {
         * L I S T E N E R S
         * *****************************
         */
-        
-        
+        document.addEventListener('keyup', app.searchEstimate)
     },
+
+    searchEstimate: (evt) => {
+        evt.preventDefault();
+        
+        let userInput = document.querySelector('.search-input').value;
+        let estimateRow = document.querySelectorAll('.estimate-row');
+        let filter = userInput.toLowerCase();
+
+        for (i = 0; i < estimateRow.length; i++) {
+            if (!estimateRow[i].innerHTML.toLowerCase().includes(filter)) {
+                estimateRow[i].style.display = "none";
+            }
+            else {
+                estimateRow[i].style.display = "";
+            }
+        }
+    }
 }
 
 // AppWitch Loading
