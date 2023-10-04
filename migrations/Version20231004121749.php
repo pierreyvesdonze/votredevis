778ad18315b42066cf1f51dbd08383b1bc2d1c2a
py eye <pierreyvesdonze.pro@gmail.com>
2023-10-04 15:22:32 +0200

    improve homepage

--- migrations/Version20231004121749.php
+++ migrations/Version20231004121749.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230106164018 extends AbstractMigration
+final class Version20231004121749 extends AbstractMigration
 {
     public function getDescription(): string
     {
